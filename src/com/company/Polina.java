9e365f1ccee5fd7c5dfacc3b0aaee5a693c22b61
Pegoxi <Pegoxi@yandex.ru>
2020-12-29 01:21:22 +0300

    first commit

---
+++ src/com/company/Polina.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Polina {
+    public static void main(String[] args) {
+        System.out.println("Полина");
+        System.out.println("18.02.2001");
+    }
+}
---
+++ src/com/company/ru/job4j/Multiple.java
@@ -0,0 +1,8 @@
+package com.company.ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println(1*2);
+        System.out.println(1*3);
+        System.out.println(1*9);}
+}
