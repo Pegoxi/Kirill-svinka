0316ed0ce2b1148239858fc907e1c8e810d9338b
Pegoxi <Pegoxi@yandex.ru>
2020-12-29 20:03:34 +0300

    5. Конвертер валюты [#244928]

--- src/com/company/ru/job4j/converter/Converter.java
+++ src/com/company/ru/job4j/converter/Converter.java
@@ -1,22 +1,22 @@
 package com.company.ru.job4j.converter;
 
 public class Converter {
-    public static int rubleToEuro(int value) {
-        int result = value / 70 ;
+    public static float rubleToEuro(float value) {
+        float result = value / 70 ;
         return result;
 
     }
 
-    public static int rubleToDollar(int value) {
-        int result = value / 60 ;
+    public static float rubleToDollar(float value) {
+        float result = value / 60 ;
         return result;
 
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(200);
-        int dollar = Converter.rubleToDollar(200);
-        System.out.println("70 rubles are" + dollar + "dollar");
-        System.out.println("60 rubles are" + euro + "euro");
+        float euro = Converter.rubleToEuro(200);
+        float dollar = Converter.rubleToDollar(200);
+        System.out.println("70 rubles are " + dollar + " dollar ");
+        System.out.println("60 rubles are " + euro + " euro ");
     }
-    }
+}
