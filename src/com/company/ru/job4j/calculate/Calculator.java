033a9f0bcd2086c30018c26295498ec2ff8955f0
Pegoxi <Pegoxi@yandex.ru>
2020-12-29 15:36:18 +0300

    3. Арифметические операции. [#244939]

---
+++ src/com/company/ru/job4j/calculate/Calculator.java
@@ -0,0 +1,16 @@
+package com.company.ru.job4j.calculate;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int six = 6;
+        int five = 5;
+        int four = 4;
+        int two = 2;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+    }
+}
