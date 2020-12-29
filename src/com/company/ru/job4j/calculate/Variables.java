9914e9c929d4a8ea79e7f576b61164ebbbcc88c0
Pegoxi <Pegoxi@yandex.ru>
2020-12-29 15:19:13 +0300

    2. Переменные. [#244934]

---
+++ src/com/company/ru/job4j/calculate/Variables.java
@@ -0,0 +1,31 @@
+package com.company.ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0332f;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+        long l = 12345611111111548l;
+        System.out.println(l);
+
+        byte age2 = 19;
+        System.out.println(age2);
+        char sex = 'w';
+        System.out.println(sex);
+        float height = 155.5f;
+        System.out.println(height);
+        long brick = 45646884895l;
+        System.out.println(brick);
+        long bacterium = 84664646645l;
+        System.out.println(bacterium);
+        double space = 46864684.6646d;
+        System.out.println("Расстояние в космосе: " + space);
+        boolean job = true;
+        System.out.println(job);
+        long fileWeight = 46446464684344686l;
+        System.out.println(fileWeight);
+    }
+}
