cfbb723aae86c92179257e091d98f0fbdcd8dfae
Pegoxi <Pegoxi@yandex.ru>
2021-01-08 21:10:54 +0300

    2. Тесты для идеальный вес [#244983]

---
+++ src/com/company/ru/job4j/calculate/FitTest.java
@@ -0,0 +1,25 @@
+package com.company.ru.job4j.calculate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        short in = 155;
+        double expected = 51.7499;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
