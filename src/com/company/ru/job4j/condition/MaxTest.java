41586087ac18116da5f70a1bb972affcce75e119
Pegoxi <Pegoxi@yandex.ru>
2021-01-09 03:52:14 +0300

    4.2. Тернарное сравнение [#244894]

---
+++ src/com/company/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,14 @@
+package com.company.ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        assertThat(Max.Max(18, 20), is(20));
+    }
+}
