ea2c8bc72a21f82a89f4107369d2612fa79e7205
Pegoxi <Pegoxi@yandex.ru>
2021-01-14 17:23:40 +0300

    5.0. Цикл for. Определение. [#244977]

---
+++ src/com/company/ru/job4j/loop/loopFor.java
@@ -0,0 +1,9 @@
+package com.company.ru.job4j.loop;
+
+public class loopFor {
+    public static void main(String[] args) {
+        for (int index = 5; index <= 10; index++) {
+            System.out.println(index);
+        }
+    }
+}
