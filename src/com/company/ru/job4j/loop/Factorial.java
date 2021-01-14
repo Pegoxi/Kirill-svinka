d606658050a5a94eb9b579918f19ecc7fd1ec63e
Pegoxi <Pegoxi@yandex.ru>
2021-01-14 20:36:53 +0300

    5.2. Создать программу, вычисляющую факториал. [#244976]

---
+++ src/com/company/ru/job4j/loop/Factorial.java
@@ -0,0 +1,12 @@
+package com.company.ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl = rsl * index;
+        }
+        return rsl;
+    }
+}
+
