f211904d6f0a17abfa66288ec7f41e55190d85b9
Pegoxi <Pegoxi@yandex.ru>
2021-01-15 12:21:42 +0300

    5.5. Простое число. [#244971]

---
+++ src/com/company/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,26 @@
+package com.company.ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when4() {
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when1() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
+}
